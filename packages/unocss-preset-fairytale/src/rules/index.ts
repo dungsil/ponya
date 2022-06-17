4efd30e6acc143b8017dfc16e9065b2cd6733c00
Kim Younggeon <mail@kyg.kr>
2022-06-17 15:48:34 +0900

    refactor(unocss-preset-fairytale): 너비/높이 유연성 추가

--- packages/unocss-preset-fairytale/src/rules/index.ts
+++ packages/unocss-preset-fairytale/src/rules/index.ts
@@ -2,49 +2,47 @@ import { type Rule, type DynamicMatcher} from '@unocss/core'
 
 const cssKey: Record<string, string> = {
   w: 'width',
-  h: 'height'
+  h: 'height',
 }
 
-function cssUnit(unit: string, wh: string): string {
+function cssUnit(unit: string = ''): string {
   switch (unit) {
+    case undefined:
+    case '':
+      return 'px'
     case 'p':
       return '%'
-    case 'v':
-      return 'v' + wh
     default:
-      return 'px'
+      return unit
   }
-}
-
-function prefix(min?: string, max?: string, maxVal?: string) {
-  if (min) {
-    return 'min-'
-  }
-
-  if (max) {
-    return 'max-'
-  }
-
-  return ''
 }
 
 export const rules: Rule<{}>[] = [
-  // `width`, `height`, `min-width`, `min-height`, `max-width`, `max-height`를 정의한다.
+  // `width`, `height`를 정의한다.
   [
-    /^([wh])(~)?(\d+)([pv])?(~)?(?:(\d+)([p|v])?)?$/,
+    /^([wh])(~)?(\d+)((?=[^~])[a-z]+)?(~(\d+)?([a-z]+)?)?$/,
     ([, wh, min, value, unit, max, maxVal, maxUnit]) => {
-
       // 이게 존재하면 min, max가 동시에 존재하는 경우로 범위 너비/높이를 지정하는 상황임
       if (maxVal) {
         return {
-          [`min-${cssKey[wh]}`]: `${value}${cssUnit(unit, wh)}`,
-          [`max-${cssKey[wh]}`]: `${maxVal}${cssUnit(maxUnit, wh)}`,
+          [`min-${cssKey[wh]}`]: `${value}${cssUnit(unit)}`,
+          [`max-${cssKey[wh]}`]: `${maxVal}${cssUnit(maxUnit)}`,
         }
       }
 
+      // 프리픽스 추가
+      let minOrMax = ''
+
+      if (min) {
+        return 'min-'
+      }
+      if (max) {
+        return 'max-'
+      }
+
       return {
-        [`${prefix(min, max)}${cssKey[wh]}`]: `${value}${cssUnit(unit, wh)}`,
+        [`${minOrMax}${cssKey[wh]}`]: `${value}${cssUnit(unit)}`,
       }
     }
-  ]
+  ],
 ]
