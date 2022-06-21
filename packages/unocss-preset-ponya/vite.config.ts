3865d2b52fa1dd145bc08abd03e261767f58ee6f
Kim Younggeon <mail@kyg.kr>
2022-06-21 17:37:57 +0900

    feat(unocss-preset-ponya): vitest 설정 추가

--- packages/unocss-preset-ponya/vite.config.ts
+++ packages/unocss-preset-ponya/vite.config.ts
@@ -3,12 +3,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import unocss from 'unocss/vite'
 
-import { presetPonya } from './src'
-
 export default defineConfig({
-  plugins: [unocss({
-    presets: [presetPonya()],
-  })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
