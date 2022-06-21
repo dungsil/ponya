31957a3632d7fc60286c67fdc76231f6913eb6e0
Kim Younggeon <mail@kyg.kr>
2022-06-21 15:34:14 +0900

    fix(unocss-preset-ponya): 오타 수정

--- packages/unocss-preset-ponya/src/preflights/index.ts
+++ packages/unocss-preset-ponya/src/preflights/index.ts
@@ -13,7 +13,7 @@ export const preflights: Preflight<Theme>[] = [
           font-weight: ${theme.font.weight[theme.base.font.weight]};
           font-size: ${theme.font.size[theme.base.font.size]};
           line-height: ${theme.lineHeight[theme.base.lineHeight]};
-          letter-spacing ${theme.letterSpacing[theme.base.letterSpacing]};
+          letter-spacing: ${theme.letterSpacing[theme.base.letterSpacing]};
           color: ${colorResolver(theme, theme.base.font.color)};
       }
     `
