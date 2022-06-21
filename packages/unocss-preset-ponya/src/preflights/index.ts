13803a8539f5c40d32c6a6a846f2ea31097eb2b9
Kim Younggeon <mail@kyg.kr>
2022-06-21 17:37:25 +0900

    refactor(unocss-preset-ponya): preflights 레이어에 공백 제거

--- packages/unocss-preset-ponya/src/preflights/index.ts
+++ packages/unocss-preset-ponya/src/preflights/index.ts
@@ -8,14 +8,14 @@ export const preflights: Preflight<Theme>[] = [
     layer: 'preflights',
     // language=CSS
     getCSS: ({ theme }) =>  `
-      body {
-          font-family: ${theme.font.family[theme.base.font.family]};
-          font-weight: ${theme.font.weight[theme.base.font.weight]};
-          font-size: ${theme.font.size[theme.base.font.size]};
-          line-height: ${theme.lineHeight[theme.base.lineHeight]};
-          letter-spacing: ${theme.letterSpacing[theme.base.letterSpacing]};
-          color: ${colorResolver(theme, theme.base.font.color)};
-      }
+body {
+  font-family: ${theme.font.family[theme.base.font.family]};
+  font-weight: ${theme.font.weight[theme.base.font.weight]};
+  font-size: ${theme.font.size[theme.base.font.size]};
+  line-height: ${theme.lineHeight[theme.base.lineHeight]};
+  letter-spacing: ${theme.letterSpacing[theme.base.letterSpacing]};
+  color: ${colorResolver(theme, theme.base.font.color)};
+}
     `
   }
 ]
